e2d048311f267cc6acfd6aa2a9ac2bb65133f899
201508876PMH <201508876PMH@gmail.com>
2020-12-10 10:13:48 +0100

    sniffing ICMP packets

--- sniff-n-spoof.py
+++ sniff-n-spoof.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
 
-import scapy.all as scapy
-
+from scapy.all import *
 
 def spoof_reply(pkt):
-    print(pkt)
-    
+    if(pkt[2].type == 8):
+        print("Creating spoof packet...")
+        dst = pkt[1].dst
+        src = pkt[1].src
+        seq = pkt[2].seq
+        id = pkt[2].id
+        load = pkt[3].load
+        reply = IP(src=dst, dst=src)/ICMP(type=0, id=id, seq=seq)/load
+        send(reply)
 
-if __name__=="__main__"
-    iface = ""
+
+
+if __name__=="__main__":
+
+    # define the network interface
+    iface = "ens18"
+
+    # filter for only ICMP trafic
+    filter = "icmp"
+
+    # start sniffing
+    sniff(iface=iface, prn=spoof_reply, filter=filter)
